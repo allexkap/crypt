920e9a21943a3350e25392e3a4bb7455fb86563c
allexkap <aekpstn@gmail.com>
2024-05-20 16:09:21 +0300

    add task four.two

--- task4.py
+++ task4.py
@@ -13,7 +13,31 @@ def lfsr(data, poly):
             return i + 1, output[::-1]
 
 
+def rc4(key, n, m):
+    key_len = len(key)
+    s = [i for i in range(n)]
+    output = []
+
+    j = 0
+    for i in range(n):
+        j = (j + s[i] + key[i % key_len]) % n
+        s[i], s[j] = s[j], s[i]
+    s0 = s.copy()
+
+    j = 0
+    for i in range(m):
+        i = (i + 1) % n
+        j = (j + s[i]) % n
+        s[i], s[j] = s[j], s[i]
+        t = (s[i] + s[j]) % n
+        output.append(s[t])
+
+    return s0, output[::-1]
+
+
 data = [1, 1, 0, 1, 0, 0]
 poly = (6, 5, 3, 1)
-
 print(*lfsr(data, poly))
+
+key = (7, 0, 9, 2, 4, 1, 3, 8, 5, 6)
+print(*rc4(key, 10, 5))
